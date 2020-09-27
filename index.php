d886ecc443c8ec0ca087c65a2faec64c8b966a3a
MorphSight <irishsnowday@icloud.com>
2020-09-26 20:52:37 -0700

    Test

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php include_once("index.html");
+<?php 
 
- echo "This works"; ?>
+include_once("index - Copy.html")
+include_once("topbar.html");
+include_once("index.html");"
+
+ echo "</div>
+</body>
+</html>"; ?>
