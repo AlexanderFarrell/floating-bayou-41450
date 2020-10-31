aa307c9762e89df99ff5609c9405444ab9a00f5c
MorphSight <irishsnowday@icloud.com>
2020-10-30 22:01:14 -0700

    as

--- GameProject/_App/RedisInit.php
+++ GameProject/_App/RedisInit.php
@@ -23,10 +23,14 @@ class RedisInit
 
         session_start();*/
 
-        if($_ENV['REDIS_URL']) {
-            $redisUrlParts = parse_url($_ENV['REDIS_URL']);
-            ini_set('session.save_handler','redis');
-            ini_set('session.save_path',"tcp://$redisUrlParts[host]:$redisUrlParts[port]?auth=$redisUrlParts[pass]");
+        if (!isset($_SESSION)){
+            if($_ENV['REDIS_URL']) {
+                $redisUrlParts = parse_url($_ENV['REDIS_URL']);
+                ini_set('session.save_handler','redis');
+                ini_set('session.save_path',"tcp://$redisUrlParts[host]:$redisUrlParts[port]?auth=$redisUrlParts[pass]");
+            }
+
+            session_start();
         }
     }
 }
